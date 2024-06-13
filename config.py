ab635a7bc0dea76f3c81490687164d963824db95
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-06-13 23:05:21 +0300

    generate_daily_report

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-mail = "ivan@mail.ru" # Адрес отправителя
-passwd = "1edQ34dzq"  # Пароль от почты отправителя
+mail = "ivan@mail.ru"       # Адрес отправителя
+passwd = "1edQ34dzq"        # Пароль от почты отправителя
 to_mails = ["sidorov@ya.ru", "liea@list.ru", "viktor@rambler.ru", "sasha@list.ru"]  # Список адресов получателей
 attachment_path = r"C:\cachemem.png"  # Путь к вложению файла в письмо
