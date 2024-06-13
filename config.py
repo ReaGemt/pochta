b74fd7854325cccf294e2751669413c99aef94d2
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-06-13 22:36:33 +0300

    newfile

---
+++ config.py
@@ -0,0 +1,3 @@
+mail = "ivan@mail.ru" # Адрес отправителя
+passwd = "1edQdzq"  # Пароль от почты отправителя                                  # Порт IMAP сервера для SSL
+to_mails = ["sidorov@ya.ru", "liea@list.ru", "viktor@rambler.ru", "sasha@list.ru"]  # Список адресов получателей
